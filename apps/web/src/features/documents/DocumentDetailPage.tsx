45f1b469f93b550ca8a8abdb886cc6bf0e11f7d9
Mateusz Choma <chomamateusz@gmail.com>
2026-08-11 12:58:37 +0200

    fix: match detail kebab icon size to table rows

--- apps/web/src/features/documents/DocumentDetailPage.tsx
+++ apps/web/src/features/documents/DocumentDetailPage.tsx
@@ -86,7 +86,7 @@ const DownloadIcon = () => (
 );
 
 const MoreVertIcon = () => (
-  <SvgIcon>
+  <SvgIcon fontSize="small">
     <path d="M12 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
   </SvgIcon>
 );
